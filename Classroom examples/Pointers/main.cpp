cdcf5ed18bceecdf7963310da9b262a7216b179c
Carson White <carsonsw09@gmail.com>
2023-03-08 14:49:19 -0700

    Worked on pointers today

---
+++ Classroom examples/Pointers/main.cpp
@@ -0,0 +1,27 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cout << num1_ptr << " " << *num1_ptr << endl;
+num1 = 16;
+cout << num1_ptr << " " << *num1_ptr << endl;
+*num1_ptr = 102;
+cout << num1_ptr << " "
