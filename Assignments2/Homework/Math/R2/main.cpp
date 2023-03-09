3dbd5e458002caddc784271489d5c13da4cb70b3
Carson White <carsonsw09@gmail.com>
2023-03-09 14:53:36 -0700

    jg

---
+++ Assignments2/Homework/Math/R2/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+
+using namesapce std; 
+
+Int main(){ 
+
+int r1, s, r2; 
+
+cin >> r1 >> s; 
+
+r2 = 2*s-r1; 
+
+cout << r2; 
+
+return 0; 
+
+} 
+
+ 
---
+++ Assignments2/Homework/Math/Triangle/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+    float b, c, d area, s, p; 
+    cout<<"Enter the length of side 1 of the triangle: "; 
+    cin>>b; 
+    cout<<"Enter the length of side 2 of the triangle: "; 
+    cin>>c; 
+
+Cout<<”Enter the length of side 3 of the triangle: “; 
+    s = (b+c+d)/2; 
+
+area = sqrt(s*(s-a)(s-b)(s-c)); 
+    cout<<"\nArea = "<<area; 
+    cout<<endl; 
+
+P = s*2; 
+
+Cout <<”\\nThe area of the triangle is = “ <<area; 
+
+cout <<endl; 
+
+cout <<”\\nThe perimeter of the triangle is = “ <<p; 
+
+cout<<endl; 
+
+ 
+    return 0; 
+} 
